eaff6d8548d1522dc3328f182f3f80b8b8a5232e
Hugo Sanchez <hds53@txstate.edu>
2019-10-02 22:46:19 -0500

    Currency Name Changes
    Updated to ensure that both currencies are displayed

--- src/Controller.java
+++ src/Controller.java
@@ -26,8 +26,8 @@ public class Controller implements MouseListener {
     public void mousePressed(MouseEvent e){
 
         if(SwingUtilities.isLeftMouseButton(e)){
-            player.updateBalance(1);
-            view.label.setText("Balance: " + String.valueOf(player.getBalance()));
+            player.updateWorkHours(1);
+            view.workLabel.setText("Hours Worked: " + String.valueOf(player.getWorkHours()));
         } else if(SwingUtilities.isRightMouseButton(e)){
 
         }
@@ -47,7 +47,7 @@ public class Controller implements MouseListener {
 
     // main
     public static void main(String[] args) throws Exception{
-        playMusic("Mario.wav");
+        playMusic("Mario.wav");      
         Controller game = new Controller();
     }
 
--- src/View.java
+++ src/View.java
@@ -4,9 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
-    private int balance;
-    JLabel label = new JLabel("Balance: " + String.valueOf(balance), SwingConstants.CENTER);
-    
+    private int workHours;
+    private int money;
+
+    JLabel workLabel = new JLabel("Hours worked: " + String.valueOf(workHours));
+    JLabel moneyLabel = new JLabel("Dollars: $" + String.valueOf(money));
+
     JButton jobButton = new JButton("Do job");
     JButton powerButton = new JButton("Power up!");
     JButton shopButton = new JButton("Buy from shop");
@@ -30,17 +33,27 @@ public class View extends JFrame implements ActionListener {
         public void paintComponent(Graphics g) {
             revalidate();
             controller.update(g);
-            balance = controller.player.getBalance();
+            workHours = controller.player.getWorkHours();
         }
     }
 
     public View(Controller c)throws Exception{
-        int balance = c.player.getBalance();
+        int workHours = c.player.getWorkHours();
+        int money = c.player.getMoney();
 
         setTitle("Vorgon Clicker!");
         setSize(750,750); // subject to change
         getContentPane().add(new MyPanel(c), BorderLayout.CENTER);
-        getContentPane().add(label, BorderLayout.PAGE_START);
+
+
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.darkGray);
+        workLabel.setForeground(Color.white);
+        moneyLabel.setForeground(Color.white);
+        topPanel.add(workLabel, BorderLayout.WEST);
+        topPanel.add(moneyLabel, BorderLayout.CENTER);
+
+        getContentPane().add(topPanel, BorderLayout.PAGE_START);
 
         // begin side menu
         GridBagLayout sideLayout = new GridBagLayout();
@@ -84,7 +97,7 @@ public class View extends JFrame implements ActionListener {
         gbc.gridwidth = 2;
         powerFlavorLabel.setForeground(Color.white);
         sidePanel.add(powerFlavorLabel, gbc);
-        
+
         gbc.gridwidth = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
@@ -98,7 +111,7 @@ public class View extends JFrame implements ActionListener {
         gbc.gridwidth = 2;
         shopFlavorLabel.setForeground(Color.white);
         sidePanel.add(shopFlavorLabel, gbc);
-        
+
         getContentPane().add(sidePanel, BorderLayout.EAST);
         // end side menu
 
